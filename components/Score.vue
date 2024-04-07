adc1064d9ba2223e377e0748fd6f008a69cb8b34
Aymeric Breton <aymericbreton8535@gmail.com>
2024-04-07 23:51:53 +0200

    rollback icon size

--- components/Score.vue
+++ components/Score.vue
@@ -2,7 +2,7 @@
   <div class="w-full flex justify-center items-center gap-12">
     <UIcon
       name="i-heroicons-question-mark-circle"
-      class=" text-nevada-500"
+      class="text-4xl text-nevada-500"
     />
     <UProgress
       class="w-4/6"
