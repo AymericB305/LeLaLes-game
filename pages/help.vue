fb3c9d1f77f92168c9485a7f925b7534e18820eb
Aymeric Breton <aymericbreton8535@gmail.com>
2024-04-10 20:52:05 +0200

    help layout wip

--- pages/help.vue
+++ pages/help.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    
+  <div class="flex flex-col justify-between py-8 px-8 h-full text-nevada-500">
+    <div>
+      <UButton
+        icon="i-heroicons-x-circle"
+        class="text-4xl !text-nevada-500"
+        variant="link"
+        size="2xl"
+        to="/home"
+      />
+      <div class="flex flex-col justify-center items-center gap-2 pt-12">
+        WIP
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 
 </script>
-
-<style>
-
-</style>
